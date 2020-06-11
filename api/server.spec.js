258d115dc6a4ec831286d16d5ce08029c4c45d01
Seth Nelson <seth.nelson@outlook.com>
2020-06-10 19:53:56 -0700

    added files and code for guided project

---
+++ api/server.spec.js
@@ -0,0 +1,31 @@
+const request = require('supertest');
+const server = require('./server');
+
+describe('server.js', () => {
+    it('should be the testing environment', () => {
+        expect(process.env.DB_ENV).toBe('testing');
+    });
+
+    describe('GET /', () => {
+        // return code
+        // return type
+        // body shape/structure
+
+        it('should return 200 OK status', () => {
+            return request(server).get('/')
+                .then(res => {
+                    expect(res.status).toBe(200);
+                });
+        });
+
+        it('should return 200 OK using async/await', async () => {
+            const res = await request(server).get('/');
+            expect(res.status).toBe(200);
+        });
+
+        it('should return {api:"up"}', async () => {
+            const res = await request(server).get('/');
+            expect(res.body).toEqual({ api: 'up' });
+        } )
+    });
+});
